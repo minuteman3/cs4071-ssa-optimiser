2589cb5ff9e34867166284edbd8338796b198729
Miles <miles@milesmcguire.com>
2013-12-07 23:24:43 +0000

    Add a main function with a nice UNIX-y file passing interface that errors out if the input can't be parsed as JSON

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python
+import argparse
+import json
+from sys import stderr
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input',
+                        nargs='?',
+                        type=argparse.FileType('r'),
+                        default='-')
+
+    args = parser.parse_args()
+
+    try:
+        infile = json.loads(args.input.read())
+    except ValueError:
+        print >> stderr, "ERROR: Invalid JSON passed as input"
+
 
 if __name__ == "__main__":
     main()
