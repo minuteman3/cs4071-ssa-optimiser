ef75f6197c7966119ffa5f1dad6a7c31c95da257
Miles McGuire <miles@milesmcguire.com>
2013-12-17 13:52:10 +0000

    Comments and minor refactorings

--- src/optimization.py
+++ src/optimization.py
@@ -5,15 +5,42 @@ from collections import defaultdict
 FOLDABLE_OPS = ["MUL","SUB","RSB","ADD"]
 NO_SIDE_EFFECTS = ["MOV","ADD","SUB","RSB","MUL"]
 
-def do_op(op, val1, val2):
+"""
+Switch statement used by constant folding optimization, instructing the
+optimizer how to fold an operation correctly.
+
+Parameter `op` should be statement["op"] from the code, and all arguments
+in `vals` should be of type int.
+
+Throws TypeError if all vals are not ints.
+"""
+def do_op(op, *vals):
+    if not all(isinstance(val, int) for val in vals):
+        raise TypeError
     return {
-        "MUL": val1 * val2,
-        "SUB": val1 - val2,
-        "RSB": val2 - val1,
-        "ADD": val1 + val2
+        "MUL": vals[0] * vals[1],
+        "SUB": vals[0] - vals[1],
+        "RSB": vals[1] - vals[0],
+        "ADD": vals[0] + vals[1]
     }.get(op, None)
 
 
+"""
+Driving function for the Simple (non-conditoinal) Constant Propagation
+algorithm specified in the SSA Optimization Algorithms handout.
+
+Transforms `code` in place with three transformations:
+    * Eliminates phi functions where all phi-function operands are equal,
+      replacing such functions with a copy operation.
+    * Constant folds operations on two constant variables, eg.
+        ADD R0, #1, #5
+      By performing the specified operation and replacing the statement
+      with a copy operation with the result, eg.
+        ADD R0, #1, #5  --->  MOV R0, #6
+    * Copy propagation taking single argument phi functions or copy assignments
+      of the form x <- Phi(y) or x <- y, deleting them and replacing all uses
+      of `x` by `y`.
+"""
 def constant_propagation(code):
     worklist = []
     for block in code["blocks"]:
@@ -39,6 +66,10 @@ def is_constant_phi(statement):
     return (statement["op"] == "phi" and
             all(op == operands[0] for op in operands))
 
+"""
+Updates a statement in place, converting a phi function into a copy operation.
+Corrupts code unless `is_constant_phi(statement)` is true.
+"""
 def convert_phi_to_copy(statement):
     val = statement["src1"]
     for src in [x for x in statement if x.startswith("src")]:
@@ -46,21 +77,44 @@ def convert_phi_to_copy(statement):
     statement["op"] = "MOV"
     statement["src1"] = val
 
+"""
+Performs constant folding in place. For an operation to be successfully folded
+three predicates must be true:
+
+    * All src parameters for the statement must be constant values.
+    * statement["op"] must be in FOLDABLE_OPS
+    * statement["op"] must have a case in `do_op`
+
+If any of these predicates are false calling fold_constant(statement) will have
+no effect on `statement`.
+"""
 def fold_constant(statement):
-    val1 = int(statement["src1"][1:])
-    val2 = int(statement["src2"][1:])
+    try:
+        val1 = int(statement["src1"][1:])
+        val2 = int(statement["src2"][1:])
+    except ValueError:
+        return
     const = do_op(statement["op"], val1, val2)
     if const is not None:
         statement["op"] = "MOV"
         statement["src1"] = "#" + str(const)
         del statement["src2"]
 
+"""
+True if `val` is a constant literal.
+"""
 def is_constant_val(val):
     return val[0] == '#'
 
+"""
+True if `val` is a variable. Always true if `is_constant_val` is false.
+"""
 def is_var(val):
-    return val[0] == 'R'
+    return val[0] != '#'
 
+"""
+True if statement is a copy operation, ie. statement["op"] == "MOV.
+"""
 def is_copy(statement):
     return statement["op"] == "MOV"
 
@@ -76,6 +130,10 @@ def propagate_constant(code, worklist, statement):
                     if statement not in worklist:
                         worklist.append(statement)
 
+"""
+Modifies `code` in place to delete statement. Can throw KeyError if code passed
+is not well formed.
+"""
 def remove_statement(code, statement):
     for block in code["blocks"]:
         for i, s in enumerate(block["code"]):
@@ -83,6 +141,24 @@ def remove_statement(code, statement):
                 del block["code"][i]
                 return
 
+"""
+Builds a list of all variables in `code` containing the following information
+for each variable:
+
+    {
+        "def_site": {
+            "block": Name of block where this variable was defined,
+            "statement": Index of the statement in which this variable is
+                         defined within the "code" field of the block specified
+                         by "block".
+        },
+        "uses": List of locations at which this variable is used of the form:
+                [{
+                    "block": As above,
+                    "statement": As above
+                }]
+    }
+"""
 def get_variables(code):
     variables = defaultdict(dict)
     for block in code["blocks"]:
@@ -100,13 +176,18 @@ def get_variables(code):
     for v in variables:
         if "uses" not in variables[v]:
             variables[v]["uses"] = []
-
     return variables
 
+"""
+Simple (non-aggressive) dead-code elimination using the algorithm from
+the SSA Optimization Algorithms handout.
+
+Transforms `code` in place, eliminating statements defining variables
+that are never used.
+"""
 def dead_code_elimination(code):
     variables = get_variables(code)
     worklist = variables.keys()
-
     while len(worklist):
         v = worklist.pop(0)
         if not len(variables[v]["uses"]):
