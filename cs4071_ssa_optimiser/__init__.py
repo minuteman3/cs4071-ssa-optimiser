0a0a91b6d73e5cac8e80280f924fe7b6479a9c8c
Miles McGuire <miles@milesmcguire.com>
2014-01-04 18:33:39 +0000

    Update to track HEAD of master

--- cs4071_ssa_optimiser/__init__.py
+++ cs4071_ssa_optimiser/__init__.py
@@ -1,9 +1,12 @@
 from .constant_propagation import constant_propagation
 from .dead_code_elimination import dead_code_elimination
 from .ssa import toSSA
+from .fromSSA import fromSSA
+
 
 def optimise(code):
     toSSA(code)
     constant_propagation(code)
     dead_code_elimination(code)
+    fromSSA(code)
     return code
