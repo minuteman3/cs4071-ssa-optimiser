4b2298f44735ae063f4daa3de7d471e14ff350a6
Miles McGuire <miles@milesmcguire.com>
2014-01-15 19:15:04 +0000

    Add aggressive dead code elimination to optimise pass

--- cs4071_ssa_optimiser/__init__.py
+++ cs4071_ssa_optimiser/__init__.py
@@ -2,11 +2,15 @@ from .constant_propagation import constant_propagation
 from .dead_code_elimination import dead_code_elimination
 from .ssa import toSSA
 from .fromSSA import fromSSA
+from .aggressive_dead_code_elimination import aggressive_dead_code_elimination
+import json
 
 
 def optimise(code):
     toSSA(code)
     constant_propagation(code)
     dead_code_elimination(code)
+    aggressive_dead_code_elimination(code)
+    constant_propagation(code)
     fromSSA(code)
     return code
